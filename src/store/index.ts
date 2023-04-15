864f4e5c917ced695f22095acb2eb1b7f62e125e
unknown <michaelmolina679@gmail.com>
2023-04-15 13:41:49 -0600

    aprendiendo el uso de los middlewares

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import usersReducer from './users/slice';
 
+// este middleware va a ser el encargado de que persistan los datos a traves de la ejecucion
+const persistanceLocalStorageMiddleware = (store) => (next) => (action) => {
+    console.log(store.getState());
+    console.log(action);
+    next(action);
+    console.log(store.getState());
+}
+
 export const store = configureStore({
     reducer: {
         users: usersReducer,
-    }
+    },
+    middleware: [persistanceLocalStorageMiddleware]
 })
 
 
